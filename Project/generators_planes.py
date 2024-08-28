c65dbc411e96310f9b276e0c1c1da4d4d457f530
root <stentbike7@gmail.com>
2024-08-28 22:50:43 +0200

    add part of daemon of generators planes

--- Project/generators_planes.py
+++ Project/generators_planes.py
@@ -7,7 +7,18 @@ from threading import Thread
 class GeneratorPlanes:
     def __init__(self):
         self.number_of_flight = 0
-        # zrob deamona i automatyczne generowanie samolotow
+        daemon_generators = Thread(target=self.generators, daemon=True)
+        daemon_generators.start()
+        self.def_while()
+
+    def def_while(self):
+        while True :
+            pass
+
+    def generators(self):
+        while True:
+            print("DEAMON")
+            time.sleep(2)
 
     def add_manual_planes(self):
         add_planes = -1
@@ -29,4 +40,8 @@ class GeneratorPlanes:
 
 if __name__ == '__main__':
     generator = GeneratorPlanes()
-    generator.add_manual_planes()
+    # generator.add_manual_planes()
+
+# TODO
+# dorobic deamona automatycznie dodawanie klientow
+# testy
